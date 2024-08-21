bbdbf6f2cc536032ec0fe39351958944c52e3a14
Dorene07 <dstmarth@gmail.com>
2024-08-20 23:14:42 -0400

    functions for all basic math operators completed

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function add(addend, addendend){
+    let sum = addend + addendend;
+    return sum;
+}
+console.log(add(3,2))
+
+function subtract(minuend, subtrahend){
+    let difference = minuend - subtrahend;
+    return difference
+}
+console.log(subtract(3,2))
+
+function multiply(multiplicand, multiplier){
+    let product = multiplicand * multiplier;
+    return product;
+}
+console.log(multiply(3,2))
+
+function divide(dividend, divisor){
+    let quotient = dividend/divisor;
+    return quotient;
+}
+console.log(divide(3,2))

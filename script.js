8a7efc96b2ce0a771a9d2fd6c515c18dec10cb7a
Dorene07 <dstmarth@gmail.com>
2024-08-21 21:12:21 -0400

    calculate function created. User can input numbers and operate

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 
-function add(addend, addendend){
-    let sum = addend + addendend;
+function add(firstNum, secondNum){
+    let sum = firstNum + secondNum;
     return sum;
 }
 console.log(add(3,2))
 
-function subtract(minuend, subtrahend){
-    let difference = minuend - subtrahend;
+function subtract(firstNum, secondNum){
+    let difference = firstNum - secondNum;
     return difference
 }
 console.log(subtract(3,2))
 
-function multiply(multiplicand, multiplier){
-    let product = multiplicand * multiplier;
+function multiply(firstNum, secondNum){
+    let product = firstNum * secondNum;
     return product;
 }
 console.log(multiply(3,2))
 
-function divide(dividend, divisor){
-    let quotient = dividend/divisor;
+function divide(firstNum, secondNum){
+    let quotient = firstNum / secondNum;
     return quotient;
 }
 console.log(divide(3,2))
+
+firstNumber = 1
+operator = '+'
+secondNumber = 2
+
+function calculate (num1, operator, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    }else if (operator === '-') {
+        return subtract(num1, num2);
+    }else if (operator === '*') {
+        return multiply(num1, num2);
+    }else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return "Error: " ;
+    }
+}
+console.log(calculate(8,"*",9))

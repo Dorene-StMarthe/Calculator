25f75b9c1c3dbc044f3a73c387a290abf1df1f2b
Dorene07 <dstmarth@gmail.com>
2024-08-22 20:49:02 -0400

    display can populate numbers

--- script.js
+++ script.js
@@ -3,29 +3,27 @@ function add(firstNum, secondNum){
     let sum = firstNum + secondNum;
     return sum;
 }
-console.log(add(3,2))
+//console.log(add(3,2))
 
 function subtract(firstNum, secondNum){
     let difference = firstNum - secondNum;
     return difference
 }
-console.log(subtract(3,2))
+//console.log(subtract(3,2))
 
 function multiply(firstNum, secondNum){
     let product = firstNum * secondNum;
     return product;
 }
-console.log(multiply(3,2))
+//console.log(multiply(3,2))
 
 function divide(firstNum, secondNum){
     let quotient = firstNum / secondNum;
     return quotient;
 }
-console.log(divide(3,2))
+//console.log(divide(3,2))
 
-firstNumber = 1
 operator = '+'
-secondNumber = 2
 
 function calculate (num1, operator, num2) {
     if (operator === '+') {
@@ -40,4 +38,17 @@ function calculate (num1, operator, num2) {
         return "Error: " ;
     }
 }
-console.log(calculate(8,"*",9))
+//console.log(calculate(8,"*",9))
+
+let displayValue = "";
+
+function appendNumber(number){
+    displayValue += number; //converts display string to number
+    updateDisplay();
+}
+
+function updateDisplay(){
+    document.querySelector('.display').textContent = displayValue;
+}
+
+updateDisplay();
